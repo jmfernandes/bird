2a23e3fe02f2b9bce2c1b1b7c45fd80ef14eaf3b
anonymatt <42722219+anonymatt@users.noreply.github.com>
2019-05-31 13:11:43 -0900

    Modified averageAndFilter to add arguments
    Added weight and filterweight

--- AlalaFunctions.py
+++ AlalaFunctions.py
@@ -26,17 +26,17 @@ def cleanAndExit():
     print("Bye!")
     sys.exit()
     
-def averageAndFilter(A):
-    X = A[0:-1] #Create Array C, that is all the values of A except for the last one because the last value is zero sometimes
+def averageAndFilter(weightdata,weight,filterweight):
+    X = weightdata[0:-1] #Create Array C, that is all the values of A except for the last one because the last value is zero sometimes
     B=len(X) #Create List B, that is equal to the length of X
-    Y=[200] #Seed our y function with an estimate of the bird weight (200 was used because of calibration weight)
+    filterweight=[200] #Seed our y function with an estimate of the bird weight (200 was used because of calibration weight)
     dt=0.1 # 10 hz is how fast we are taking time, every 0.1 seconds
     freq=1 #frequency of transients we want to reject (ones faster than 1 hz or 1 second)
     omega=freq*2*3.1415 #omega for low pass filter, converting to radians
     if B > 2:
         for n in range(1,B):
             yi=(Y[n-1]+X[n]*dt*omega)/(1+omega*dt)
-            Y.append(yi)
-        print(Y[B-1])
-        birdWeight = sum(X)/B #Find the average weight by dividing the sum of C by the length of C (which is B)
-        print(birdWeight) 
+            filterweight.append(yi)
+        print(filterweight[B-1])
+        weight = sum(X)/B #Find the average weight by dividing the sum of C by the length of C (which is B)
+        print(weight) 
