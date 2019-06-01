6b9e81a0492021f5b50b356aea2e020f3f162b3c
jmfernandes <joshfernandes@mac.com>
2019-06-01 10:55:37 -0700

    fixed website

--- hackathon/birds/views.py
+++ hackathon/birds/views.py
@@ -8,14 +8,13 @@ import csv
 
 from .models import Feeding
 # Functions
-def enterValue(rfid,datetime,gps,birdweight,foodweight,temperature,humidity,windspeed,airquality,rain,video,pic1,pic2,pic3,pic4):
-    q = Feeding(RFID=rfid,datetime=datetime,GPS=gps,birdweight=birdweight,foodweight=foodweight,temperature=temperature,
-    humidity=humidity,windspeed=windspeed,airquality=airquality,rain=rain,video=video,picture1=pic1,picture2=pic2,picture3=pic3,picture4=pic4)
+def enterValue(rfid,datetime,gps,hoppername,hopperweight,birdweight,feedingduration,feedingamount,temperature,rainamount,filepath):
+    q = Feeding(RFID=rfid,datetime=datetime,GPS=gps,hoppername=hoppername,hopperweight=hopperweight,
+    birdweight=birdweight,feedingduration=feedingduration,feedingamount=feedingamount,temperature=temperature,rainamount=rainamount,filepath=filepath)
     q.save()
 # Create your views here.
 def index(request):
     latest_feeding_list = Feeding.objects.order_by('-datetime')
-    template = loader.get_template('birds/index.html')
     context = {
         'latest_feeding_list' : latest_feeding_list
     }
@@ -30,58 +29,46 @@ def clear(request):
 
 def submit(request):
     if request.method == 'GET':
-        return render(request, 'birds/enter.html')
+        return render(request, 'birds/enter.html',{'headernames': [f.name for f in Feeding._meta.get_fields() if f.name != "id"]})
     else:
         try:
             RFID = request.POST['RFID']
             datetime = request.POST['datetime']
             GPS = request.POST['GPS']
+            hoppername = request.POST['hoppername']
+            filepath = request.POST['filepath']
+            hopperweight = request.POST['hopperweight']
             birdweight = request.POST['birdweight']
-            foodweight = request.POST['foodweight']
+            feedingduration = request.POST['feedingduration']
+            feedingamount = request.POST['feedingamount']
             temperature = request.POST['temperature']
-            humidity = request.POST['humidity']
-            windspeed = request.POST['windspeed']
-            airquality = request.POST['airquality']
-            rain = request.POST['rain']
-            video = request.POST['video']
-            pic1 = request.POST['pic1']
-            pic2 = request.POST['pic2']
-            pic3 = request.POST['pic3']
-            pic4 = request.POST['pic4']
+            rainamount = request.POST['rainamount']
             if (not RFID):
-                raise KeyError
-            if (not GPS):
                 raise KeyError
             if (not datetime):
                 raise KeyError
+            if (not GPS):
+                raise KeyError
+            if (not hoppername):
+                raise KeyError
+            if (not hopperweight):
+                raise KeyError
             if (not birdweight):
                 raise KeyError
-            if (not foodweight):
+            if (not feedingduration):
+                raise KeyError
+            if (not feedingamount):
                 raise KeyError
             if (not temperature):
                 raise KeyError
-            if (not humidity):
+            if (not rainamount):
                 raise KeyError
-            if (not windspeed):
-                raise KeyError
-            if (not airquality):
-                raise KeyError
-            if (not rain):
-                raise KeyError
-            if (not video):
-                video = " "
-            if (not pic1):
-                pic1 = " "
-            if (not pic2):
-                pic2 = " "
-            if (not pic3):
-                pic3 = " "
-            if (not pic4):
-                pic4 = " "
-            enterValue(RFID,datetime,GPS,birdweight,foodweight,temperature,humidity,windspeed,airquality,rain,video,pic1,pic2,pic3,pic4)
+            if (not filepath):
+                filepath = "None"
+            enterValue(RFID,datetime,GPS,hoppername,hopperweight,birdweight,feedingduration,feedingamount,temperature,rainamount,filepath)
             return HttpResponseRedirect(reverse('birds:index'))
         except (KeyError):
-            return render(request, 'birds/enter.html',{'error_message': "Make Sure all fields are filled out. Video and Picture paths are optional."})
+            return render(request, 'birds/enter.html',{'headernames': [f.name for f in Feeding._meta.get_fields() if f.name != "id"],'error_message': "Make Sure all fields are filled out. Filepath is optional."})
 
 def detail(request, feeding_id):
     feeding = model_to_dict(get_object_or_404(Feeding, pk=feeding_id))
@@ -93,10 +80,10 @@ def delete(request, feeding_id):
     except Feeding.DoesNotExist:
         raise Http404("Feeding ID does not exist")
     feeding.delete()
-    return HttpResponseRedirect(reverse('birds:index'))
+    return HttpResponseRedirect(reverse('birds:clear'))
 
-def enter(request, rfid,datetime,gps,birdweight,foodweight,temperature,humidity,windspeed,airquality,rain,video,pic1,pic2,pic3,pic4):
-    enterValue(rfid,datetime,gps,birdweight,foodweight,temperature,humidity,windspeed,airquality,rain,video,pic1,pic2,pic3,pic4)
+def enter(rfid,datetime,gps,hoppername,hopperweight,birdweight,feedingduration,feedingamount,temperature,rainamount,filepath):
+    enterValue(rfid,datetime,gps,hoppername,hopperweight,birdweight,feedingduration,feedingamount,temperature,rainamount,filepath)
     return HttpResponse('')
 
 def retrieve(request):
