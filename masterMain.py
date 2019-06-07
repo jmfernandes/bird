a707d3f6cc5e7bddd8d231da71b32497f8a96025
jmfernandes <joshfernandes@mac.com>
2019-06-06 22:25:06 -0900

    moved broken folder

--- masterMain.py
+++ masterMain.py
@@ -8,6 +8,7 @@ import datetime as dt
 import sys
 import serial
 import RPi.GPIO as GPIO
+import requests
 # Import third-party functions
 from mfrc522 import SimpleMFRC522
 from hx711 import HX711
@@ -47,13 +48,18 @@ dataDict['OverheadCamera2'] = "None"
 #Compression Loadcell = hxcomp1 & hxcomp2, bar load cells are hxbar1 & hxbar2
 hxcomp1= HX711(17, 27)
 hxcomp2= HX711(16, 20)
-hxcomp1.set_reference_unit(440.3) #222.55892255
+#print("load cell set up")
+hxcomp1.set_reference_unit(220.2) #222.55892255
 hxcomp1.reset()
+#print("first reset done")
 hxcomp1.tare()
-hxcomp2.set_reference_unit(1)
+#print("first tare done")
+hxcomp2.set_reference_unit(-33.9) # weight in grams
+#print("second ref done")
 hxcomp2.reset()
+#print("second reset done")
 hxcomp2.tare()
-print("Tare done! Add weight now...")
+#print("Tare done! Add weight now...")
 
 
 birdWeightList = []
@@ -65,13 +71,15 @@ baseDir = os.path.dirname(os.path.realpath(__file__))
 cameraPath = "{0}/media".format(baseDir)
 databasePath = "{0}/test.db".format(baseDir)
 usbPath = "/media/pi/1842-ED03/csv/"
+camera1dev = "video0"
+camera2dev = "video2"
 
 #===============================================================================#
 
 #=============================== Set up GPIO Pins ==============================#
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 ledPin = 21 
-buttonPin = 7 
+buttonPin = 26 
 servoPin = 24 #which pin is the servo connected to (besides power and gnd)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(ledPin,GPIO.OUT, initial=0)
@@ -81,7 +89,7 @@ p1 = GPIO.PWM(servoPin, 50) #sets 50hz frequency for servoPin
 #===============================================================================#
 
 def my_callback(channel):
-    GPIO.output(21,1)
+    GPIO.output(ledPin,1)
     print('Callback')
     try:
         upload_data_to_database(dataList)
@@ -91,18 +99,24 @@ def my_callback(channel):
         print('Error writing data to USB stick')
         for i in range(3):
             time.sleep(0.25)
-            GPIO.output(21,0)
+            GPIO.output(ledPin,0)
             time.sleep(0.25)
-            GPIO.output(21,1)
+            GPIO.output(ledPin,1)
         
-    GPIO.output(21,0)
+    GPIO.output(ledPin,0)
 
-GPIO.add_event_detect(7, GPIO.RISING, callback=my_callback, bouncetime=300)
+GPIO.add_event_detect(buttonPin, GPIO.RISING, callback=my_callback, bouncetime=300)
 
-
+#count = 0
+#blah = []
 while running:
+    print(requests.get("http://google.com"))
+    sys.exit()
     try:
-        val = int(hxcomp1.get_weight(5))
+        #val = int(hxcomp1.get_weight(1))
+        val = int(hxcomp1.get_weight(1))
+        #blah.append(val)
+        #print(sum(blah)/len(blah))
         currentTime = dt.datetime.now()
         timeString = currentTime.strftime('%Y-%m-%d %H:%M:%S')
         if (currentTime.hour >= 21):
@@ -116,17 +130,18 @@ while running:
             print("value of {}".format(val))
             time.sleep(0.1)
         else:
-            id,text = reader.read()
+            id,text = reader.read_my_id()
+            #id = "josh"
             if id:
                 # Get initial data
                 dataDict['RFID']= id
                 dataDict['datetime']= timeString
                 dataDict['filePath'] = 'https://www.dropbox.com/home/media/{}/{}'.format(id,timeString)
                 # Open hood
-                actuateServo(p1,50)
+                actuateServo(p1,60)
                 # Take pictures
-                dataDict['RightSideCamera1'] = TakeUSBPicture1(cameraPath,id,timeString,"RightSideCamera1") #video0          
-                dataDict['LeftSideCamera1'] = TakeUSBPicture2(cameraPath,id,timeString,"LeftSideCamera1") #video1              
+                dataDict['RightSideCamera1'] = TakeUSBPicture1(cameraPath,id,timeString,"RightSideCamera1",camera1dev) #video0          
+                dataDict['LeftSideCamera1'] = TakeUSBPicture2(cameraPath,id,timeString,"LeftSideCamera1",camera2dev) #video1              
                 dataDict['OverheadCamera1'] = TakePiPicture(cameraPath,id,timeString,"OverheadCamera1")
                 # Get the weight of the bird
                 for n in range(1,30):
@@ -135,8 +150,6 @@ while running:
                     time.sleep(.1)
                 dataDict['birdWeight']=lowPassFilter(birdWeightList)
                 birdWeightList.clear()
-                # Take video
-                dataDict['video'] = TakeVideo(cameraPath,id,timeString,"video")
                 # Get the hopper weight
                 for n in range(1,30):
                     val = hxcomp2.get_weight(1)
@@ -144,33 +157,50 @@ while running:
                     time.sleep(.1)
                 dataDict['hopperWeight']=lowPassFilter(hopperWeightList)
                 hopperWeightList.clear()
+                # Take video
+                dataDict['video'] = TakeVideo(cameraPath,id,timeString,"video")
                 # Take second round of photos
-                dataDict['RightSideCamera2'] = TakeUSBPicture1(cameraPath,id,timeString,"RightSideCamera2") #video0
-                dataDict['LeftSideCamera2'] = TakeUSBPicture2(cameraPath,id,timeString,"LeftSideCamera2") #video1
+                dataDict['RightSideCamera2'] = TakeUSBPicture1(cameraPath,id,timeString,"RightSideCamera2",camera1dev) #video0
+                dataDict['LeftSideCamera2'] = TakeUSBPicture2(cameraPath,id,timeString,"LeftSideCamera2",camera2dev) #video1
                 dataDict['OverheadCamera2'] = TakePiPicture(cameraPath,id,timeString,"OverheadCamera2")
                 # Get temperature and rain
-                file = open('./weather.csv','r')
-                reader = csv.reader(file)
-                tempList = list(reader)[-1]
-                dataDict['temperature'] = tempList[1]
-                dataDict['rainAmount'] = tempList[0]
-                file.close()
+                file = None
+                try:
+                    file = open('./weather.csv','rb')
+                    csvReader = csv.reader(file)
+                    tempList = list(csvReader)[-1]
+                    dataDict['temperature'] = tempList[1]
+                    dataDict['rainAmount'] = tempList[0]
+                    file.close()
+                except:
+                    if file:
+                        file.close()
+                # Close the lid
+                while id:
+                    id,text = reader.read_my_id()
+                    time.sleep(5)
+                actuateServo(p1,-45)
+                if (dataDict['RightSideCamera2'] == "None" and dataDict['LeftSideCamera2'] == "None" and dataDict['OverheadCamera2'] == "None" and
+                    dataDict['RightSideCamera1'] == "None" and dataDict['LeftSideCamera1'] == "None" and dataDict['OverheadCamera1'] == "None" and
+                    dataDict['video'] == "None"):
+                    dataDict['filePath'] = "None"
+                # Get feeding duration
+                dataDict['feedingDuration'] = (dt.datetime.now()-currentTime).total_seconds()
+                for n in range(1,30):
+                    val = hxcomp2.get_weight(1)
+                    hopperWeightList.append(val)
+                    time.sleep(.1)
+                dataDict['feedingAmount'] = dataDict['hopperWeight'] - lowPassFilter(hopperWeightList)
+                hopperWeightList.clear()
                 print(dataDict)
                 # Append to List
                 dataList.append(dataDict)
-                # Close the lid
-                while id:
-                    id,text = reader.read()
-                actuateServo(p1,-45)
-                # Get feeding duration
-                dataDict['feedingDuration'] = (dt.datetime.now()-currentTime).total_seconds()
                 sys.exit()
 
     except (KeyboardInterrupt,SystemExit):
         upload_images_to_dropbox(dataList)
         upload_data_to_database(dataList)
         upload_data_to_website(dataList)
-        #cleanAndExit()
         GPIO.cleanup()
         running = False
 
